e495708a46a592616cc697362fd0e412508a950a
Robin Scott <robinscott@btinternet.com>
2015-04-29 14:56:22 +0100

    Removed items for bolierplate

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,12 @@
 module.exports = function(grunt) {
 
-    // 1. All configuration goes here 
+    // Configurations
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        compass: {
-            dist: {
-                options: {
-                    sassDir: 'sass',
-                    cssDir: 'stylesheets',
-                    outputStyle: 'expanded'
-                }
-            }
-        },
         watch: {
             all: {
                 files: ['sass/**/*.scss'],
-                tasks: ['compass'],
                 options: {
                     spawn: false
                 }
@@ -42,13 +32,13 @@ module.exports = function(grunt) {
 
     });
 
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    //grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-browser-sync');
+    // Load tasks
+    // grunt.loadNpmTasks('grunt-contrib-sass');
+    // grunt.loadNpmTasks('grunt-contrib-watch');
+    // grunt.loadNpmTasks('grunt-contrib-concat');
+     grunt.loadNpmTasks('grunt-browser-sync');
 
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['compass', 'browserSync', 'watch']);
+    // Register tasks
+    grunt.registerTask('default', ['watch']);
 
 };
